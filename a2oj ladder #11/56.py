93c65addfe8676b5ce9b509aff91107224b7d9e7
lorenaleao <42307217+lorenaleao@users.noreply.github.com>
2019-02-08 13:51:23 -0200

    Update 56.py

--- a2oj ladder #11/56.py
+++ a2oj ladder #11/56.py
@@ -1,29 +1,30 @@
 import collections as c
+from collections import defaultdict
 		
 line0 = input()
 line0 = [int(i) for i in line0.split()]
 
 kirito_strength = line0[0]
 
-dragons = {}
+dragons = defaultdict(list)
 
 for i in range(line0[1]):
 	line = input()
 	line = [int(i) for i in line.split()]
-	dragons[line[0]] = line[1] #line[0] -> dragon's strength
+	dragons[line[0]].append(line[1]) #line[0] -> dragon's strength
 							   #line[1] -> the bonus for defeating it
 
 dragons = c.OrderedDict(sorted(dragons.items()))
-for k, v in dragons.items(): print(k, v)
-#WORKS ONLY IF THE DRAGONS HAVE DIFFERENT STRENGTHS (KEYS)
+#for k, v in dragons.items(): print(k, v)
 
 kirito_dies = False
 for strength in dragons:
-	if strength < kirito_strength:
-		kirito_strength += dragons[strength]
-	else:
-		kirito_dies = True 
-		break
+	for elem in dragons[strength]:
+		if strength < kirito_strength:
+			kirito_strength += elem
+		else:
+			kirito_dies = True 
+			break
 
 if kirito_dies:
 	print("NO")
